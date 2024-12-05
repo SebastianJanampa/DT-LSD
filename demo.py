87635cb8444d16697ee7548035e12a5cde8880c0
SebastianJanampa <rjsebastian001@gmail.com>
2024-12-05 13:36:14 -0700

    upload demo

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@ import os, sys
 import numpy as np
 import torch
 import cv2
+import argparse
+
 import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 200
 import torchvision.transforms.functional as functional
@@ -10,6 +12,12 @@ from models import build_model
 from util.slconfig import SLConfig
 from util.misc import nested_tensor_from_tensor_list
 from PIL import Image
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    return parser
 
 
 class Compose(object):
@@ -82,74 +90,94 @@ class Resize(object):
         size = self.sizes
         return resize(img, size, self.max_size)
 
-model_config_path = "config/DINO/DINO_4scale_swin.py" # change the path of the model config file
-model_checkpoint_path = "logs/1100/SWIN-MS4-36-epochs/checkpoint0035.pth" # change the path of the model checkpoint
+def main(args):
+    model_config_path = "config/DTLSD/DTLSD_4scale_swin.py" # change the path of the model config file
+    model_checkpoint_path = "DTLSD_checkpoint0035.pth" # change the path of the model checkpoint
 
-# obtain checkpoints
-checkpoint = torch.load(model_checkpoint_path)
+    cfg = SLConfig.fromfile(model_config_path)
+    cfg_dict = cfg._cfg_dict.to_dict()
+    for k,v in cfg_dict.items():
+        setattr(args, k, v)
 
-# load model
-args = checkpoint['args']
-model, _, postprocessors = build_model(args)
-model.load_state_dict(checkpoint['model'])
-model.to('cuda')
-model.eval()
+    # obtain checkpoints
+    checkpoint = torch.load(model_checkpoint_path)
 
-PLTOPTS = {"color": "#33FFFF", "s": 6, "edgecolors": "none", "zorder": 5}
-# load image
-files = ['00031841', '00035228', 'P1080074', 'P1080116']
-for file in files:
+    # create folder to save results
     try:
-        raw_img = Image.open(f'./figures/{file}.png').convert("RGB")
-    except:
-        raw_img = Image.open(f'./figures/{file}.jpg').convert("RGB")
-    w, h = raw_img.size
-    orig_size = torch.as_tensor([int(h), int(w)])
-
-    # normalize image
-    test_size = 1100
-    normalize = Compose([
-        ToTensor(),
-        Normalize([0.538, 0.494, 0.453], [0.257, 0.263, 0.273]),
-        Resize([test_size]),
-    ])
-    img = normalize(raw_img)
-    inputs = nested_tensor_from_tensor_list([img])
+        os.mkdir('results')
+        print("Directory 'results' created successfully.")
+    except FileExistsError:
+        print("Directory 'results' already exists.")
 
 
-    with torch.no_grad():
-        outputs = model(inputs.to("cuda"))
+    # load model
+    model, _, postprocessors = build_model(args)
+    model.load_state_dict(checkpoint['model'])
+    model.to('cuda')
+    model.eval()
 
-    out_logits, out_line = outputs['pred_logits'][0].cpu(), outputs['pred_lines'][0].cpu()
-    prob = out_logits.sigmoid()
+    PLTOPTS = {"color": "#33FFFF", "s": 6, "edgecolors": "none", "zorder": 5}
 
-    scores, labels = prob[..., :-1].max(-1)
+    # load image
+    files = ['00031723', '00031797', 'P1080018', 'P1080074']
+    for file in files:
+        try:
+            raw_img = Image.open(f'./samples/{file}.png').convert("RGB")
+        except:
+            raw_img = Image.open(f'./samples/{file}.jpg').convert("RGB")
+        w, h = raw_img.size
+        orig_size = torch.as_tensor([int(h), int(w)])
 
-    img_h, img_w = orig_size.unbind(0)
-    scale_fct = torch.unsqueeze(torch.stack([img_w, img_h, img_w, img_h], dim=0), dim=0)
-    lines = out_line #* scale_fct[:, None, :]
-    lines = lines.view(900, 2, 2)
-    lines = lines.flip([-1])# this is yxyx format
-    lines = lines.detach().numpy()
-    scores = scores.detach().numpy()
-    keep = scores >= 0.25
-    keep = keep.squeeze()
-    lines = lines[keep]
-    lines = lines.reshape(lines.shape[0], -1)
-    extent=[0, 1, 1, 0]
+        # normalize image
+        test_size = 1100
+        normalize = Compose([
+            ToTensor(),
+            Normalize([0.538, 0.494, 0.453], [0.257, 0.263, 0.273]),
+            Resize([test_size]),
+        ])
+        img = normalize(raw_img)
+        inputs = nested_tensor_from_tensor_list([img])
 
-    plt.figure(figsize=(5, 5))
-    plt.imshow(raw_img, extent=extent)
 
-    for tp_id, line in enumerate(lines):
-        y1, x1, y2, x2 = line # this is yxyx
-        p1 = (x1, y1)
-        p2 = (x2, y2)
-        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linewidth=1.5, color='darkorange', zorder=1)
-        plt.scatter(p1[0], p1[1], **PLTOPTS)
-        plt.scatter(p2[0], p2[1], **PLTOPTS)
-    plt.axis([0, w-1, h-1, 0])
-    plt.axis('off')
-    plt.axis('equal')
-    plt.savefig(f'./figures/{file}_VIS.png', pad_inches=0.1, bbox_inches='tight')
-    # plt.show()
+        with torch.no_grad():
+            outputs = model(inputs.to("cuda"))
+
+        out_logits, out_line = outputs['pred_logits'][0].cpu(), outputs['pred_lines'][0].cpu()
+        prob = out_logits.sigmoid()
+
+        scores, labels = prob[..., :-1].max(-1)
+
+        img_h, img_w = orig_size.unbind(0)
+        scale_fct = torch.unsqueeze(torch.stack([img_w, img_h, img_w, img_h], dim=0), dim=0)
+        lines = out_line #* scale_fct[:, None, :]
+        lines = lines.view(900, 2, 2)
+        lines = lines.flip([-1])# this is yxyx format
+        lines = lines.detach().numpy()
+        scores = scores.detach().numpy()
+        keep = scores >= 0.25
+        keep = keep.squeeze()
+        lines = lines[keep]
+        lines = lines.reshape(lines.shape[0], -1)
+        extent=[0, 1, 1, 0]
+
+        plt.figure(figsize=(5, 5))
+        plt.imshow(raw_img, extent=extent)
+
+        for tp_id, line in enumerate(lines):
+            y1, x1, y2, x2 = line # this is yxyx
+            p1 = (x1, y1)
+            p2 = (x2, y2)
+            plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linewidth=1.5, color='darkorange', zorder=1)
+            plt.scatter(p1[0], p1[1], **PLTOPTS)
+            plt.scatter(p2[0], p2[1], **PLTOPTS)
+        plt.axis([0, w-1, h-1, 0])
+        plt.axis('off')
+        plt.axis('equal')
+        plt.savefig(f'./results/{file}_VIS.png', pad_inches=0.1, bbox_inches='tight')
+        # plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('DTLSD', parents=[get_args_parser()])
+    args = parser.parse_args()
+    main(args)
